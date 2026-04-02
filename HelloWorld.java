0417c9efd250a4f4f35c3a37ee9a9894d3fcb340
ghanacharan2007-dev <ghanacharan2007@gmail.com>
2026-04-02 14:04:16 +0530

    UC6: Format multiple names using substring method

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,9 +4,17 @@ public class HelloWorld {
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
+
+            String result = "";
+
             for (String name : args) {
-                System.out.println("Hello " + name);
+                result += name + ", ";
             }
+
+            // Remove last ", "
+            result = result.substring(0, result.length() - 2);
+
+            System.out.println("Hello " + result);
         }
 
     }
