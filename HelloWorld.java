7a68fa1a4171129cfd79a4c20bd1e4de953f4377
ghanacharan2007-dev <ghanacharan2007@gmail.com>
2026-04-02 13:53:06 +0530

    UC3: Display name with default value

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
 
-        if(args.length > 0) {
-            System.out.println("Hello " + args[0]);
-        } else {
-            System.out.println("Hello World");
+        String name = "World";  // default value
+
+        if (args.length > 0) {
+            name = args[0];
         }
 
+        System.out.println("Hello " + name);
     }
 }
